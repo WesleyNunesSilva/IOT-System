1f4b907615dd72cb440cbb41fb11963c9e0f4370
Wesley Nunes <wesleynunes990@gmail.com>
2023-04-04 21:31:59 -0300

    add global styles

---
+++ styles/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    :root {
+        font-size: 62.5%;
+    }
+
+    body {
+        font-size: 1.6rem;
+    }
+
+    input, button, label, select, a {
+        font-family: 'Poppins', sans-serif;
+        border: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover, a:hover {
+        filter: brightness(0.9);
+    }
+`;
+
---
+++ styles/theme.js
@@ -0,0 +1,22 @@
+export default {
+    colors: {
+      bg_blue_600: "#078493",
+      bg_blue_500: "#0A99AB",
+
+      orange_400: "#FF9000",
+      orange_500:'#FF5C00',
+      orange_600: '#E36901',
+
+      bg_red_600: "#DB0000",
+      bg_red_800: '#6B0000',
+      bg_red_700: '#B10E0E',
+
+      status_ok: '#00B41D',
+      status_alert:'#6B0000',
+      status_attention:'#3D3705',
+
+      bg_alert:'',
+      bg_attention:'#BEC106',
+
+    }
+  }
